564c3163a5ae5bbfed3233a535cbab7811aa02dc
TaryckC <157592049+TaryckC@users.noreply.github.com>
2024-10-09 16:27:46 +0200

    Add files via upload

--- GaussianElimination.c
+++ GaussianElimination.c
@@ -1,63 +1,77 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
-struct Matrix
-{
-    double** Matrix;
-    int l;
-    int c;
+void gaussian_elimination(double** A, double* b, int n) {
+    for (int i = 0; i < n; ++i) {
+        int j = i;
 
-};
+        // Trouver un pivot non nul
+        while (j < n && A[j][i] == 0) {
+            j++;
+        }
 
-void init_matrix(struct Matrix *m) {
-    m->Matrix = (double**) malloc(sizeof(double*)*m->l);
-    for (int i=0; i<m->l; ++i) {
-        m->Matrix[i] = (double*) malloc(sizeof(double)*m->c);
-    }
-}
+        // Si aucun pivot n'a été trouvé, il n'y a pas de solution unique
+        if (j == n) {
+            printf("No unique solution\n");
+            return;
+        }
 
-void fillMatrix(double* tab, struct Matrix *matrix){
-    int v1 = 0;
-    //printf("%d", matrix->l * matrix->c);
-    // (double) sizeof(tab)/sizeof(double) ==  (double) matrix->l * matrix->c
-    if(true) {
-        for (int i=0; i<sizeof(tab)/sizeof(double); ++i) {
-            
-            if (i%matrix->c == 0 && v1 != 0){
-                v1++;
+        // Élimination de Gauss pour rendre les éléments en dessous du pivot égaux à zéro
+        for (int j = i + 1; j < n; ++j) {
+            double factor = A[j][i] / A[i][i];
+            b[j] -= factor * b[i];
+
+            for (int k = i; k < n; ++k) {
+                A[j][k] -= factor * A[i][k];
             }
-            matrix->Matrix[v1][i%matrix->c] = tab[i];
-            //printf("%d", tab[i]);
-            
         }
     }
-}
 
-void printMatrix(struct Matrix *m){
-    for (int i=0; i< m->l; ++i){
-        for (int j=0; j< m->c; ++j){
-                //printf("test");
-                //printf("%s", &m.Matrix[i][j] == NULL);
-                printf("%lf", (m->Matrix[i][j]));
-            }
+    // Affichage de la matrice triangulaire supérieure et du vecteur b
+    printf("Upper triangular matrix A:\n");
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            printf("%lf ", A[i][j]);
+        }
         printf("\n");
     }
+
+    printf("Vector b:\n");
+    for (int i = 0; i < n; ++i) {
+        printf("%lf ", b[i]);
+    }
+    printf("\n");
 }
 
 int main() {
+    int n = 3;
 
-    struct Matrix matrix;
-    matrix.c = 3;
-    matrix.l = 3;
+    // Allocation de la matrice A
+    double** A = (double**)malloc(n * sizeof(double*));
+    for (int i = 0; i < n; ++i) {
+        A[i] = (double*)malloc(n * sizeof(double));
+    }
 
-    init_matrix(&matrix);
+    // Initialisation de la matrice A
+    A[0][0] = 2;  A[0][1] = 1;  A[0][2] = -1;
+    A[1][0] = -3; A[1][1] = -1; A[1][2] = 2;
+    A[2][0] = -2; A[2][1] = 1;  A[2][2] = 2;
 
-    double tab[9] = {1,2,3,4,5,6,7,8,9};
-    //printf("%d", sizeof(tab)/sizeof(double));
+    // Allocation et initialisation du vecteur b
+    double* b = (double*)malloc(n * sizeof(double));
+    b[0] = 8;
+    b[1] = -11;
+    b[2] = -3;
 
-    fillMatrix(tab, &matrix);
-    printMatrix(&matrix);
-    
+    // Appel de la fonction d'élimination de Gauss
+    gaussian_elimination(A, b, n);
+
+    // Libération de la mémoire
+    for (int i = 0; i < n; ++i) {
+        free(A[i]);
+    }
+    free(A);
+    free(b);
+
     return 0;
 }
